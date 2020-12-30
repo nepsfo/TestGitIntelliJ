54cdbae7024c6f12d466e49a94f0b3f44fce43a7
NY Apparel <nepsfo@gmail.com>
2020-12-29 20:11:04 -0800

    "2 new lines"

--- src/com/mycompany/Main.java
+++ src/com/mycompany/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Testing Git");
         System.out.println("Adding more lines");
+        System.out.println("New User Adding Comments");
+        System.out.println("Test Me");
+
     }
 }
